6753525eb087f9584abd29fbe9c4e15bb569cb17
Parlegrandpa <adebiyipaul1@gmail.com>
2019-02-18 09:33:51 +0100

    removed unwanted comments

--- src/shared/Backend.js
+++ src/shared/Backend.js
@@ -2,28 +2,18 @@ const baseUrl = 'https://api.github.com/search/repositories?q=language:Java&sort
 
 export const SERVER_REQUEST = async (body, reqType) => {
     let response;
-    // const _headers = await GET_HEADER();
     const url = `${baseUrl}`;
 
     if (reqType.toLowerCase() === 'get') {
         response = await fetch(url, {
             method: reqType.toUpperCase(),
-            // headers: _headers
         });
     } else {
         response = await fetch(url, {
             method: reqType.toUpperCase(),
-            // headers: _headers,
             body: JSON.stringify(body)
         });
     }
 
     return await response.json();
 };
-
-// const GET_HEADER = async () => {
-//     return new Headers({
-//         'Content-Type': 'application/json',
-//         'x-access-token': await GET_TOKEN(),
-//     });
-// };
